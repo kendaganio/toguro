690e45525a56e3cb75cefe8bc0a3252ed05fd136
Ken-Lauren Daganio <akosiken@gmail.com>
2018-02-22 13:49:57 +0800

    Test/demo page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import Mustache from 'mustache'
+
 const whatInputType = {
 // 'json-schema': 'input[type=]'
   'string': 'text',
@@ -5,53 +7,82 @@ const whatInputType = {
   'integer': 'number'
 }
 
+function getRenderer (type) {
+  const renderers = {
+    label (key, value) {
+      const template = `<label class='{{key}}-label' for='{{ for }}'> {{ title }} </label>`
+
+      const div = document.createElement('div')
+      const rendered = Mustache.render(template, {
+        ...value,
+        key,
+        for: `${key}-field`
+      })
+
+      div.innerHTML = rendered
+      return div.firstElementChild
+    },
+
+    text (key, value) {
+      const input = document.createElement('input')
+      input.setAttribute('type', whatInputType[value.type])
+      input.setAttribute('id', `${key}-field`)
+      return input
+    },
+
+    checkbox (key, value) {
+      this['text'](key, value)
+    },
+
+    number (key, value) {
+      this['text'](key, value)
+    }
+  }
+
+  return renderers[type]
+}
+
 const createFormElement = (key, value) => {
-  const label = document.createElement('label')
-  label.innerText = key
-  label.setAttribute('for', `${key}-field`)
-
-  const input = document.createElement('input')
-  input.setAttribute('type', whatInputType[value.type])
-  input.setAttribute('id', `${key}-field`)
-
   const div = document.createElement('div')
-  div.appendChild(label)
-  div.appendChild(input)
+  div.className = 'form-group'
+  div.appendChild(getRenderer('label')(key, value))
+  div.appendChild(getRenderer('text')(key, value))
 
   return div
 }
 
-export const render = (el, { schema: { properties }, submitHandler}) => {
-
+export const render = (el, { schema: { properties }, submitHandler }) => {
   // init validations
   if (!(el instanceof Element)) {
     throw new Error('el should be a valid HTML Element')
   }
 
-
   const form = document.createElement('form')
+  form.setAttribute('class', 'form')
+
+  const fieldSet = document.createElement('fieldset')
 
   // createElements
   Object.keys(properties).forEach(key => {
     console.log(key, properties[key])
     const inputEl = createFormElement(key, properties[key])
-    form.insertAdjacentElement('beforeend', inputEl)
+    fieldSet.insertAdjacentElement('beforeend', inputEl)
   })
 
   // add submit button
   const submitButton = document.createElement('input')
   submitButton.setAttribute('type', 'submit')
-  submitButton.value = 'submit'
+  submitButton.className = 'button -primary'
+  submitButton.value = 'Submit!'
 
-  form.insertAdjacentElement('beforeend', submitButton)
+  fieldSet.insertAdjacentElement('beforeend', submitButton)
+  form.insertAdjacentElement('beforeend', fieldSet)
   //
   el.insertAdjacentElement('beforeend', form)
-
 
   // createEventHandlers
   form.onsubmit = (event) => {
     event.preventDefault()
-    console.log('hijacked');
-    submitHandler('walang data');
+    submitHandler('walang data')
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,8 @@ module.exports = {
       use: [{
         loader: 'babel-loader',
         options: {
-          presets: ['babel-preset-env']
+          presets: ['@babel/preset-env'],
+          plugins: [require('@babel/plugin-proposal-object-rest-spread')]
         }
       }, {
         loader: 'standard-loader',
