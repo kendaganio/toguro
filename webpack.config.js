258028990f77e3ec4438db5fd9ec6118d9a13ca1
Ken-Lauren Daganio <akosiken@gmail.com>
2018-02-22 11:09:04 +0800

    Setups webpack and babel

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'toguro.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: 'toguro',
+    libraryTarget: 'umd'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: [{
+        loader: 'babel-loader',
+        options: {
+          presets: ['babel-preset-env']
+        }
+      }, {
+        loader: 'standard-loader',
+        options: {
+          snazzy: true
+        }
+      }]
+    }],
+  }
+}
